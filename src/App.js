cf7b1dd209598e2a2bb65ac1e2ab38b969177470
Ajithkumar1810 <jaganajith18@gmail.com>
2022-07-26 15:29:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import './index.css';
+import Loginform  from './components/Loginform';
 
 function App() {
+  const adminUser={
+    name:"admin",
+    email :"admin@admin.com",
+    password :"admin123"
+  }
+  const [user,setUser]=useState({name:"",email:"",password:""});
+  const [error,setError]=useState("");
+  const Login=details=>{
+    console.log(details);
+    if(details.email==adminUser.email &&details.password==adminUser.password && details.name==adminUser.name)
+    {
+      // console.log("Login sucessfull")
+      setUser({name:details.name,email:details.email,password:details.password})
+    }
+    else if(details.name!=adminUser.name){
+      setError("Name Does not Match")
+    }
+    else if(details.email!=adminUser.email){
+      setError("Email Does not Match")
+    }
+    else {
+      setError("Password Does not Match")
+    }
+    
+   
+  }
+  const Logout=()=>{
+    // console.log("Logout");
+    setUser({name:"",email:"",password:""})
+    setError("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {(user.email!="")? (
+      <div className="welcome">
+        <h2>Welcome,<span>{user.name}</span></h2>
+        <button onClick={Logout}>Logout</button>
+      </div>
+     ):(
+      <Loginform Login={Login} error={error}/>
+     )}
     </div>
   );
 }
